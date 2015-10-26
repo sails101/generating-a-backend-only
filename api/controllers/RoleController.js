e2b7efd4bad1aafd934070c03fc7f046e2e733bf
Travis Webb <me@traviswebb.com>
2015-10-26 12:26:24 -0400

    created user and role models

---
+++ api/controllers/RoleController.js
@@ -0,0 +1,11 @@
+/**
+ * RoleController
+ *
+ * @description :: Server-side logic for managing Roles
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/models/Role.js
@@ -0,0 +1,21 @@
+/**
+* Role.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+    name: {
+      type: 'string'
+    },
+
+    users: {
+      collection: 'User',
+      via: 'roles'
+    }
+  }
+};
+
